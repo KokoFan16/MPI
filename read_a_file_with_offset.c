292e8bb87830fa85b73d84b0d3d4b1ce66eac315
KokoFan16 <koko14fan@gmail.com>
2019-09-19 16:29:00 -0500

    Update read_a_file_with_offset.c

--- read_a_file_with_offset.c
+++ read_a_file_with_offset.c
@@ -1,1 +1,27 @@
+#include "mpi.h"
 
+#define FILESIZE (1024 * 1024)
+
+int main(int argc, char **argv)
+{
+  int *buf, rank, nprocs, nints, bufsize;
+  MPI_File fh;
+  MPI_Status status;
+  
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+  
+  bufsize = FILESIZE/nprocs; // int bytes
+  buf = (int *) malloc(bufsize);
+  nints = bufsize/sizeof(int); // the number of ints
+  offset = rank * bufsize; // offset for each process
+  
+  MPI_File_open(MPI_COMM_WORLD, "testfile", MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
+  // offset is passed as an argument to MPI_File_read_at
+  MPI_File_read_at(fh, offset, buf, nints, MPI_INT, &status);
+  MPI_File_close(&fh);
+  
+  free(buf);
+  MPI_Finalize();
+  return 0;
