e03c4b31fbbf8c96ce2c354f806101ebc8c3454c
KokoFan16 <koko14fan@gmail.com>
2019-09-20 10:49:13 -0500

    Update read_noncontiguous_block.c

--- read_noncontiguous_block.c
+++ read_noncontiguous_block.c
@@ -1,1 +1,19 @@
 
+#include "mpi.h"
+
+int main(int argc, char **argv)
+{
+  MPI_INT 1b, extent;
+  MPI_Datatype etype, filetype, contig;
+  MPI_Offset disp;
+  MPI_File fh;
+  int *buf;
+  
+  
+  
+  buf = (int *)malloc(1000 * sizeof(int));
+  
+  
+  
+  
+}
